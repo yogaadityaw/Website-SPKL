b65a59e7f10af963a60683580f0296ba2ddd286c
ifkusyoba <ifkusyobaa@gmail.com>
2024-05-14 20:59:35 +0700

    Slice view for detail spkl and add vertical timeline component.

--- resources/views/kabeng-views/detail-spkl.blade.php
+++ resources/views/kabeng-views/detail-spkl.blade.php
@@ -29,13 +29,69 @@
                                     <h6 class="card-title">Detail Pengajuan Lembur</h6>
                                 </div>
                                 <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-3">
+                                            <h5>Nama PT</h5>
+                                            <p>PT. ALREDHO TEKNIK</p>
+                                        </div>
+                                        <div class="col-3">
+                                            <h5>Bengkel</h5>
+                                            <p>Bengkel Meki</p>
+                                        </div>
+                                        <div class="col-3">
+                                            <h5>Jam Mulai Lembur</h5>
+                                            <p>-</p>
+                                        </div>
+                                        <div class="col-3">
+                                            <h5>Rencana</h5>
+                                            <p>Pengembangan sistem deteksi awal untuk kapal perang baru dengan fokus
+                                                pada integrasi sensor baru dan pengujian kemampuan anti-serangan
+                                                rudal</p>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-3">
+                                            <h5>Nomor Pengajuan</h5>
+                                            <p>{{$spkls->spkl_number}}</p>
+                                        </div>
+                                        <div class="col-3">
+                                            <h5>Departemen</h5>
+                                            <p>Departemen Subject</p>
+                                        </div>
+                                        <div class="col-3">
+                                            <h5>Jam Akhir Lembur</h5>
+                                            <p>-</p>
+                                        </div>
+                                        <div class="col-3">
+                                            <h5>Uraian Target Lembur</h5>
+                                            <p>Meningkatkan efisiensi perbaikan sistem radar kapal dengan mengurangi
+                                                waktu penyelesaian sebesar 20% dalam dua bulan</p>
+                                        </div>
+                                        <div class="row ml-1 mr-1">
+                                            <div class="col-9">
+                                                <label><h5>Karyawan</h5></label>
+                                                <textarea
+                                                    class="form-control"
+                                                    data-height="150"
+                                                    required=""></textarea>
+                                            </div>
+                                            <div class="col-3">
+                                                <h5>Progress</h5>
+                                                <p>Kami telah mencapai 50% dari target pengujian dan menemukan beberapa
+                                                    potensi perbaikan yang dapat meningkatkan kinerja sistem secara
+                                                    keseluruhan.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
 
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
                 </div>
-            </div>
 
-    </div>
+            </div>
         </section>
 @endsection
---
+++ resources/views/pages/horizontal-timeline.blade.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test View</title>
+    <link rel="stylesheet" href="{{ asset('css/timeline.css') }}">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="timeline-steps aos-init aos-animate" data-aos="fade-up">
+                <div class="timeline-step">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top"
+                         title=""
+                         data-original-title="2003">
+                        <div class="inner-circle"></div>
+                        <p class="h6 mt-3 mb-1">2003</p>
+                        <p class="h6 text-muted mb-0 mb-lg-0">Favland Founded</p>
+                    </div>
+                </div>
+                <div class="timeline-step">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top"
+                         title=""
+                         data-original-title="2004">
+                        <div class="inner-circle"></div>
+                        <p class="h6 mt-3 mb-1">2004</p>
+                        <p class="h6 text-muted mb-0 mb-lg-0">Launched Trello</p>
+                    </div>
+                </div>
+                <div class="timeline-step">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top"
+                         title=""
+                         data-original-title="2005">
+                        <div class="inner-circle"></div>
+                        <p class="h6 mt-3 mb-1">2005</p>
+                        <p class="h6 text-muted mb-0 mb-lg-0">Launched Messanger</p>
+                    </div>
+                </div>
+                <div class="timeline-step">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top"
+                         title=""
+                         data-original-title="2010">
+                        <div class="inner-circle"></div>
+                        <p class="h6 mt-3 mb-1">2010</p>
+                        <p class="h6 text-muted mb-0 mb-lg-0">Open New Branch</p>
+                    </div>
+                </div>
+                <div class="timeline-step mb-0">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top"
+                         title=""
+                         data-original-title="2020">
+                        <div class="inner-circle"></div>
+                        <p class="h6 mt-3 mb-1">2020</p>
+                        <p class="h6 text-muted mb-0 mb-lg-0">In Fortune 500</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
